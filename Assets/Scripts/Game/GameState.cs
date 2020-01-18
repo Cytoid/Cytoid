ea6b3a8b1d46ddcbc003a910215509de3040320a
Tiger Tang <tigerhix@gmail.com>
2020-01-18 03:06:18 -0500

    Ranked mode multiplier bug fix

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -104,7 +104,7 @@ public class GameState
                     NoteScoreMultiplier += 0.0005D * noteScoreMultiplierFactor;
                     break;
                 case NoteGrade.Good:
-                    NoteScoreMultiplier -= -0.005D * noteScoreMultiplierFactor;
+                    NoteScoreMultiplier -= 0.005D * noteScoreMultiplierFactor;
                     break;
                 case NoteGrade.Bad:
                     NoteScoreMultiplier -= 0.025D * noteScoreMultiplierFactor;

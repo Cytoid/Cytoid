66ba767d5e4d4c2556ca3d507d7ab3583c156829
Teages <Teages@teages.xyz>
2025-08-30 08:39:54 +0800

    feat: move ratings to like/dislike
    * chore(api): add like and dislike field for rating api
    
    * feat: move ratings to like/dislike
    
    * fix: improve rating display logic in RatingTab
    
    * fix: ui offset in rating tab
    
    * fix: update rating message to include like/dislike context
    
    * refactor: rework rating message logic for clarity

--- Assets/Scripts/Navigation/Elements/RateLevelElement.cs
+++ Assets/Scripts/Navigation/Elements/RateLevelElement.cs
@@ -3,37 +3,54 @@ using UnityEngine.UI;
 
 public class RateLevelElement : MonoBehaviour
 {
-    public Text averageRatingText;
-    public Text numRatingsText;
+    public Text numMainRatingsText;
+    public Text numMinorRatingsText;
     public Text messageText;
     public InteractableMonoBehavior rateButton;
 
     private void Awake()
     {
-        averageRatingText.text = "N/A";
+        numMainRatingsText.text = "N/A";
+        numMinorRatingsText.text = "";
         messageText.text = "";
-        numRatingsText.text = "";
         rateButton.gameObject.SetActive(false);
     }
 
     public void SetModel(string levelId, LevelRating data)
     {
-        if (data.total > 0)
+        numMainRatingsText.text = data.like > 0 ? data.like.ToString("0") : "N/A";
+
+        if (data.dislike > 0)
         {
-            averageRatingText.text = ((data.average ?? 0) / 2.0).ToString("0.00");
-            numRatingsText.text = $"{data.total} " + (data.total > 1 ? "GAME_PREP_RATINGS_UNIT_RATINGS" : "GAME_PREP_RATINGS_UNIT_RATING").Get();
+            numMinorRatingsText.text = (data.dislike > 1 ? "GAME_PREP_RATINGS_RECV_DISLIKES" : "GAME_PREP_RATINGS_RECV_DISLIKE").Get(data.dislike);
+        }
+        else if (data.total > 0)
+        {
+            numMinorRatingsText.text = $"{data.total} " + (data.total > 1 ? "GAME_PREP_RATINGS_UNIT_RATINGS" : "GAME_PREP_RATINGS_UNIT_RATING").Get();
         }
         else
         {
-            averageRatingText.text = "N/A";
-            numRatingsText.text = "0 " + "GAME_PREP_RATINGS_UNIT_RATINGS".Get();
+            numMinorRatingsText.text = "0 " + "GAME_PREP_RATINGS_UNIT_RATINGS".Get();
         }
 
         if (Context.OnlinePlayer.IsAuthenticated && Context.LevelManager.LoadedLocalLevels.ContainsKey(levelId))
         {
-            messageText.text = data.rating > 0
-                ? "GAME_PREP_RATINGS_YOU_RATED_X".Get($"{data.rating / 2.0:0.#}")
-                : (data.total > 0 ? "GAME_PREP_RATINGS_YOU_HAVENT_RATED" : "GAME_PREP_RATINGS_BE_THE_FIRST").Get();
+            if (data.rating > 0)
+            {
+                messageText.text = "GAME_PREP_RATINGS_YOU_RATED_X_WITH_TYPE".Get(
+                    $"{data.rating / 2.0:0.#}",
+                    data.rating > 5
+                        ? "GAME_PREP_RATINGS_LIKE".Get()
+                        : "GAME_PREP_RATINGS_DISLIKE".Get()
+                );
+            }
+            else
+            {
+                messageText.text = data.total > 0
+                    ? "GAME_PREP_RATINGS_YOU_HAVENT_RATED".Get()
+                    : "GAME_PREP_RATINGS_BE_THE_FIRST".Get();
+            }
+
             rateButton.gameObject.SetActive(true);
         }
         else
--- Assets/Scripts/Online/LevelRating.cs
+++ Assets/Scripts/Online/LevelRating.cs
@@ -6,4 +6,6 @@ public class LevelRating
     public double? average;
     public int total;
     public int? rating;
+    public int like;
+    public int dislike;
 }
